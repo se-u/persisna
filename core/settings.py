dcd041f18d3599e106e258a78f88c42ec2dbbc3e
monster <monsterblublu@pm.me>
2021-04-01 09:58:22 +0700

    set env for production

--- core/settings.py
+++ core/settings.py
@@ -20,10 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ybkqv2z&7k^ya_b6_!ap%u($8)&wi5rm7%@t0=^(1!oj!83y1h'
+#SECRET_KEY = 'ybkqv2z&7k^ya_b6_!ap%u($8)&wi5rm7%@t0=^(1!oj!83y1h'
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'ybkqv2z&7k^ya_b6_!ap%u($8)&wi5rm7%@t0=^(1!oj!83y1h')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+#DEBUG = False
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = ['persisna.herokuapp.com']
 
