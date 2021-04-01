9c7ccd7b4101cfbdf253bc4365f55023ee288918
monster <monsterblublu@pm.me>
2021-04-01 22:09:15 +0700

    fixing settings and 404 template

--- core/settings.py
+++ core/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ybkqv2z&7k^ya_b6_!ap%u($8)&wi5rm7%@t0=^(1!oj!83y1h'
+#SECRET_KEY = 'ybkqv2z&7k^ya_b6_!ap%u($8)&wi5rm7%@t0=^(1!oj!83y1h'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'ybkqv2z&7k^ya_b6_!ap%u($8)&wi5rm7%@t0=^(1!oj!83y1h')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = False
