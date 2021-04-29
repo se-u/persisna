87b2ca53f771c0af2947a7484a57f00614af115b
monster <monsterblublu@pm.me>
2021-04-29 22:39:53 +0700

    add quiz

--- attendances/admin.py
+++ attendances/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Presence
+from .models import Presence, Quiz
 
 # Register your models here.
 admin.site.register(Presence)
+admin.site.register(Quiz)
