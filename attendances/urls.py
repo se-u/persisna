5e2e0f277204d855358ce32b9f83f57b086ba3b4
persisna <81467581+persisna@users.noreply.github.com>
2021-11-05 11:15:18 +0700

    Update urls.py

--- attendances/urls.py
+++ attendances/urls.py
@@ -6,7 +6,7 @@ from .views import (PresenceView, PresenceList,
 
 
 urlpatterns = [
-    path('klsz', PresenceView.as_view(), name='presence'),
+    path('zkmw', PresenceView.as_view(), name='presence'),
     path('quizzz/',QuizView.as_view(), name='quiz'),
     path('dashboard/', PresenceList.as_view(), name='dashboard'),
     path('search/', SearchPresence.as_view(), name='search-presence'),
